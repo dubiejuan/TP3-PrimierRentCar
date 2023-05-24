2abf8495a7126ce83c61776048fa5b3170081ce3
dubiejuan <dubiejuan@gmail.comn>
2023-05-25 08:18:39 +0900

    start proyect

---
+++ app/src/main/java/ort/edu/tp3/primierrentcar/MainActivity.kt
@@ -0,0 +1,11 @@
+package ort.edu.tp3.primierrentcar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
