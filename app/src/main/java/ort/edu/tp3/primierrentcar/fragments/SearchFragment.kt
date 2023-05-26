2fbb511b074970cbe098b95f7c4cec3d83bb9d18
dubiejuan <dubiejuan@gmail.comn>
2023-05-26 18:39:50 -0500

    navigation

---
+++ app/src/main/java/ort/edu/tp3/primierrentcar/fragments/SearchFragment.kt
@@ -0,0 +1,27 @@
+package ort.edu.tp3.primierrentcar.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ort.edu.tp3.primierrentcar.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class SearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
